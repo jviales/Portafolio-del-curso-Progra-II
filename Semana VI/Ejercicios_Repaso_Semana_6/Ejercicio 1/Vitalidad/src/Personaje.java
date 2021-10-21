11774543e4b9c1919fa13c1f6e841aaea0beba20
unknown <aoa218@gmail.com>
2021-10-20 22:52:53 -0400

    Semana 6 actualizada

---
+++ Semana VI/Ejercicios_Repaso_Semana_6/Ejercicio 1/Vitalidad/src/Personaje.java
@@ -0,0 +1,20 @@
+package Vitalidad.src;
+
+public class Personaje extends Vitalidad {
+    private String nombre;
+    private Armadura armadura;
+    public Personaje(short valor1, short valor2, short valor3, short valor4) {
+        super(valor1, valor2, valor3, valor4);
+    }
+    public Armadura getArmadura() {
+        return armadura;
+    }
+    public void setArmadura(Armadura armadura) {
+        this.armadura = armadura;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+      
+    
+}
---
+++ Semana VI/Ejercicios_Repaso_Semana_6/Ejercicio 2/Ejercicio 2/src/Rol.java
@@ -0,0 +1,12 @@
+public class Rol extends Entidad
+implements Imprimible{
+    public int getId(){return this.id;}
+    public void setId(int valor){
+        this.id = valor;}
+    @Override
+    public String imprimir() {
+        return "ID " + 
+        Integer.toString(this.id)+
+        " Rol: " + this.nombre;
+    }
+}
